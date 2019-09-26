5028a21cefbdbd3d6989a8fc1eac5e5f73da7016
Tianyu Duan <tianyuduan1991@gmail.com>
2019-09-25 18:10:33 -0700

    integrated material UI + added react-routers,switch, homepage, splash, set up register/login routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AppAppBar from './modules/views/AppAppBar';
+
+import Login from './modules/views/Login';
+import Register from './modules/views/Register';
+// import ProductSmokingHero from './modules/views/ProductSmokingHero';
+
+import withRoot from './modules/withRoot';
+import ProductHero from './modules/views/ProductHero';
+
 
 const App = () => (
-  <Fragment>
-    <h1>app</h1>
-  </Fragment>
+  <Router>
+    <Fragment>
+      <AppAppBar />
+      <Route exact path='/'component={ProductHero}/>
+      <section className='container'>
+        <Switch>
+          <Route exact path='/register' component={Register}/>
+          <Route exact path='/login' component={Login}/>
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
 )
 
-export default App;
+export default withRoot(App);
